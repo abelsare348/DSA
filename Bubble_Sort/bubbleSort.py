682a89d6913212a67848e6c52d8d1bb1f8f24603
abelsare348 <aniketbelsare348@gmail.com>
2025-05-30 11:14:15 +0530

    Added comments

--- Bubble_Sort/bubbleSort.py
+++ Bubble_Sort/bubbleSort.py
@@ -4,18 +4,21 @@ def bubbleSort(arr: list):
     swapflag=False
     j=len(arr)-1
     while(j>0):
-        for i in range(0,j,1):
+        swapflag=False  # turn back the swap flag to false after each and every pass if swapped happpen in that pass.
+        for i in range(0,j,1): # loop to check whether the element is greater than next if yes then swap
             if arr[i]>arr[i+1]:
                 temp=arr[i]
                 arr[i]=arr[i+1]
                 arr[i+1]=temp
-                swapflag=True
-                print(arr)
+                swapflag=True 
+                #print(arr)
             else:
                 continue
-        if swapflag==True:
+            # if no element found in pass which needed swap then break the sorting directly.
+        if swapflag==True: 
             j-=1
-        else:
+        else: 
             break
 
 bubbleSort(arr=arrIO)
+print(arrIO)
